b7f29aaa73059c85feb07d7224a869e920505ff6
ariel <arielshongoordi@gmail.com>
2023-08-27 21:06:34 +0200

    "espace membre"

--- views/compte.php
+++ views/compte.php
@@ -1,30 +1,14 @@
 <?php 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-use App\Redirect;
-global $router;
-if(isset($_GET['deconnexion'])){
-unset($_SESSION["auth"]);
-$redirect=new Redirect("" . $router->generate("home"));
-$redirect->go();
+global $params;
+$name=$params["name"];
+if(!empty($_SESSION["auth"])){
+    if($_SESSION["auth"]["name"] !== $name){
+        require "404.php";
+       die();  
+    }
+}else{
+    require "404.php";
+    die();
 }
 ?>
 <h1>mon compte<h1>
-<div class="modal" tabindex="-1" role="dialog" id="example">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">deconnexion</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>vous etez sur de vouloir vous deconnecter?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary"   data-bs-dismiss="modal">non fermez la boite modal</button>
-        <a type="button" class="btn btn-primary" href="?deconnexion">oui j'en suis sur</a>
-      </div>
-    </div>
-  </div>
-</div>
--- views/template/header.php
+++ views/template/header.php
@@ -1,8 +1,15 @@
 <?php
-use App\Session;
 require dirname(__DIR__,2) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
+use App\Redirect;
+use App\Session;
 $session = new Session();
 $session->start();
+global $router;
+if(isset($_GET['deconnexion'])){
+unset($_SESSION["auth"]);
+$redirect=new Redirect("" . $router->generate("home"));
+$redirect->go();
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -58,5 +65,23 @@ $session->start();
     </div>
   </div>
 </nav>
-
+<div class="modal" tabindex="-1" role="dialog" id="example">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">deconnexion</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>vous etez sur de vouloir vous deconnecter?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary"   data-bs-dismiss="modal">non fermez la boite modal</button>
+        <a type="button" class="btn btn-primary" href="?deconnexion">oui j'en suis sur</a>
+      </div>
+    </div>
+  </div>
+</div>
 

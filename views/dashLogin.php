705571d2ef1481465581185392a9f7cdc3a9b063
ariel <arielshongoordi@gmail.com>
2023-09-11 18:52:27 +0200

    "dashbord"

--- views/dashLogin.php
+++ views/dashLogin.php
@@ -1,1 +1,46 @@
-verification
+<?php 
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+use App\Getpdo;
+use App\Session;
+global $router;
+$pdo= Getpdo::connect();
+$query=$pdo->query("SELECT * FROM admin")->fetch(PDO::FETCH_OBJ);
+$success=null;
+$error=null;
+if(!empty($_POST)){
+    $mom=$_POST["nom"];
+    $password=$_POST["password"];
+    if(($mom === $query->name) && (password_verify($password,$query->password))){
+        $success= "bienvenue " . $query->name;
+    }else{
+       $error=" nom ou mot de passe incorect"; 
+    }
+}
+if($success !== null){
+  $_SESSION["dashbord"]="ok";
+  header("Location:" . $router->generate("dashbord"));
+}
+require "nav.php"?>
+<div class="container">
+<?php if ($error !== NULL):?>
+        <div class="alert alert-danger" role="alert">
+            <?=$error?>
+        </div>
+<?php endif ?>
+<?php if ($success !== NULL):?>
+        <div class="alert alert-success" role="alert">
+            <?=$success ?>
+        </div>
+<?php endif ?>
+<form action="" method="POST">
+  <div class="form-group">
+    <label for="exampleInputEmail1">nom</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" name="nom"  placeholder="votre nom" required>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="mot de passe" required>
+  </div>
+  <button type="submit" class="btn  btn-success mt-2">Submit</button>
+</form>
+</div>
--- views/dashbord.php
+++ views/dashbord.php
@@ -1,1 +1,26 @@
-<h1>mon dashbord</h1>
+<?php 
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+use App\Getpdo;
+global $router;
+if(empty($_SESSION["dashbord"])){
+    header("Location:" . $router->generate("dashLogin"));
+}
+$pdo= Getpdo::connect();
+$articles = $pdo->query("SELECT Count(id) FROM article")->fetch()[0];
+$users=$pdo->query("SELECT Count(id) FROM users")->fetch()[0];
+$users_identifie=$pdo->query("SELECT Count(id) FROM users WHERE token IS NULL")->fetch()[0];
+$users_non_identifie=($users-$users_identifie);
+$commentaires=$pdo->query("SELECT Count(id) FROM commentaires")->fetch()[0];
+$moyenne_commentaires=round($commentaires/$articles);
+require "nav.php"
+?>
+<div class="container">
+    <div class="text-center">
+        <h1>dashbord</h1>
+    </div>
+    <div class="d-flex justify-content-center">
+      <div class="bg-green mt-3 h-50 d-inline-block" style="min-width: 300px;">
+        hhhhhhhhhhhh
+      </div>
+    </div>
+</div>
